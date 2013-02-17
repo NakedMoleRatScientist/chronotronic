a5ec1da72701f5e061da42afd909a77482c3c4e4
Han Dao <hackerkiba@gmail.com>
2013-02-17 07:23:00 -0500

    display seconds escalated.

--- chronotask.js
+++ chronotask.js
@@ -1,7 +1,21 @@
 Events = new Meteor.Collection("events");
 
+String.prototype.toTime = function () {
+  sec_numb    = parseInt(this);
+  var hours   = Math.floor(sec_numb / 3600);
+  var minutes = Math.floor((sec_numb - (hours * 3600)) / 60);
+  var seconds = sec_numb - (hours * 3600) - (minutes * 60);
+
+  if (hours   < 10) {hours   = "0"+hours;}
+  if (minutes < 10) {minutes = "0"+minutes;}
+  if (seconds < 10) {seconds = "0"+seconds;}
+  var time    = hours+':'+minutes+':'+seconds;
+  return time;
+}
+
 Session.set("timer", false);
 Session.set("eventId", null);
+
 
 if (Meteor.isClient) {
   Template.nav.profile = function () {
@@ -35,7 +49,9 @@ if (Meteor.isClient) {
   Template.timer.time = function() {
     if (Session.get("eventId") != null)
     {
-      //Nothing here yet.
+      e = Events.findOne(Session.get("eventId"));
+      sec = ((new Date(e.end) - new Date(e.start)) / 1000).toString();
+      return sec.toTime();
     }
     return "00:00:00";
 
