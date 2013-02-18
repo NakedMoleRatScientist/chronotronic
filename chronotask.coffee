15b6143dd3d77c3dfdcfd9f0eaa0e01dffd8593a
Han Dao <hackerkiba@gmail.com>
2013-02-18 06:19:13 -0500

    move from chronotask.js to chronotask.js

---
+++ chronotask.coffee
@@ -0,0 +1,4 @@
+Events = new Meteor.Collection("events")
+
+Session.set("timer", false)
+Session.set("eventId", null)
--- chronotask.js
+++
@@ -1,4 +0,0 @@
-Events = new Meteor.Collection("events");
-
-Session.set("timer", false);
-Session.set("eventId", null);
