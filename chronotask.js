01b0d223d0c0a6b55882d47500b0a45509cbacf7
Han Dao <hackerkiba@gmail.com>
2013-02-17 23:21:48 -0500

    cleanup and move

--- chronotask.js
+++ chronotask.js
@@ -1,18 +1,5 @@
 Events = new Meteor.Collection("events");
 
-String.prototype.toTime = function () {
-  sec_numb    = parseInt(this);
-  var hours   = Math.floor(sec_numb / 3600);
-  var minutes = Math.floor((sec_numb - (hours * 3600)) / 60);
-  var seconds = sec_numb - (hours * 3600) - (minutes * 60);
-
-  if (hours   < 10) {hours   = "0"+hours;}
-  if (minutes < 10) {minutes = "0"+minutes;}
-  if (seconds < 10) {seconds = "0"+seconds;}
-  var time    = hours+':'+minutes+':'+seconds;
-  return time;
-}
-
 Session.set("timer", false);
 Session.set("eventId", null);
 
